ec9036cc72c2ee33bc1e16bbb325dacdb59c7c43
Yacine Mohamdi <myacine70@gmail.com>
2020-01-09 01:19:32 +0100

    fix ghost styled newline char not rendered

--- src/helpers/ctb-interpreter/index.ts
+++ src/helpers/ctb-interpreter/index.ts
@@ -5,20 +5,43 @@ import { groupNodesByLine } from './steps/group-nodes-by-line';
 import { insertOtherTables } from './steps/insert-other-tables';
 import { parseXml } from './helpers/helpers';
 
+const flagGhostLines = xmlString =>
+  xmlString.replace(/>(\s*\n\s*<\/rich_text>)/g, ' containsNewLine="true" >$1');
+const catchGhostNewlines = xml =>
+  xml.map(node => {
+    if (typeof node === 'object') {
+      if (node.$.containsNewLine) {
+        node._ = '\n';
+        delete node.$.containsNewLine;
+      }
+    }
+    return node;
+  });
+
 const parseRichText = async ({
   nodeTableXml,
   otherTables = {},
   meta: { name },
   options: { stringify }
 }) => {
-  const parsedXml = await parseXml({ xml: nodeTableXml });
+  const temp = flagGhostLines(nodeTableXml);
+  console.log({ temp });
+  const parsedXml = await parseXml({ xml: temp });
   // @ts-ignore
   const richText = parsedXml.node.rich_text;
   console.log('pre insert-other-tables', richText);
-  const xml = insertOtherTables({ xml: richText, otherTables });
+  const withNewLines = catchGhostNewlines(richText);
+  const xml = insertOtherTables({ xml: withNewLines, otherTables });
   console.log('pre css-html translator', xml);
   const translated = xml.map(node => {
+    // edgecase where richtext
+
     if (typeof node === 'object') {
+      if (node.$.containsNewLine) {
+        node._ = '\n';
+        delete node.$.containsNewLine;
+        console.log('replaced node', node);
+      }
       node.$$ = translateAttributesToHtmlAndCss(node.$);
     }
     return node;
