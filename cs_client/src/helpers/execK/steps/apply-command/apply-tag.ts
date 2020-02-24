fb814bb4f7cb46ed9d85b22f75fd09ecbfd857e4
Yacine Mohamdi <myacine70@gmail.com>
2020-02-24 17:17:31 +0100

    fix a bug in apply-tag

--- cs_client/src/helpers/execK/steps/apply-command/apply-tag.ts
+++ cs_client/src/helpers/execK/steps/apply-command/apply-tag.ts
@@ -5,9 +5,10 @@ const applyTag = ({ aHtmlElement, tagExists, tagName }) => {
       const indexOfTagToBeDeleted = aHtmlElement.tags.indexOf(toBeDeleted);
       aHtmlElement.tags.splice(indexOfTagToBeDeleted, 1);
       // merge styles of the item to be deleted with the top-level tag tag
+      if (!aHtmlElement.tags.length) aHtmlElement.tags.push(['span', {}]);
       if (toBeDeleted[1].style)
         aHtmlElement.tags[0][1].style =
-          toBeDeleted[1].style + aHtmlElement.tags[0][1].style;
+          toBeDeleted[1].style + (aHtmlElement.tags[0][1].style || '');
     }
   } else {
     if (!toBeDeleted) aHtmlElement.tags.push([tagName, {}]);
