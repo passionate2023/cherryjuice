13a9e2cc7449bbf4b581689bcdff70355ca85f51
Yacine Mohamdi <myacine70@gmail.com>
2020-02-25 15:56:44 +0100

    (execK) fix bugs in create-word-range

--- cs_client/src/helpers/execK/steps/get-selection/index.ts
+++ cs_client/src/helpers/execK/steps/get-selection/index.ts
@@ -7,29 +7,38 @@ const getLineDiv = el =>
   el.parentElement.classList.contains('rich-text__line')
     ? el.parentElement
     : getLineDiv(el.parentElement);
-const mapNodesToTextLength = xs =>
+const mapNodesToText = xs =>
   xs.map(el =>
-    el.nodeType === 1
-      ? el.innerText.length
-      : el.nodeType === 3
-      ? el.wholeText.length
-      : 0
+    el.nodeType === 1 ? el.innerText : el.nodeType === 3 ? el.wholeText : ''
   );
-const findAnchors = ({ line, startOffset, endOffset }) =>
-  mapNodesToTextLength(getLineChildren(line)).reduce(
+const mapTextToLength = xs => xs.map(str => str.length);
+const findAnchors = ({ line, startOffset, endOffset }) => {
+  return mapNodesToText(getLineChildren(line)).reduce(
     (acc, val, i) => {
-      acc.tl += val;
-      if (startOffset < acc.tl && acc.s === -1) acc.s = i;
-      if (endOffset <= acc.tl && acc.e === -1) acc.e = i;
+      const l = val.length;
+      if (startOffset < acc.tl + l && acc.s === -1) {
+        acc.s = i;
+        // acc.so = i > 0 ? acc.tl + l - startOffset : startOffset;
+        acc.so = i > 0 ? startOffset - acc.tl : startOffset;
+      }
+      if (endOffset <= acc.tl + l && acc.e === -1) {
+        acc.e = i;
+        // acc.eo = acc.tl + l + 1 - endOffset;
+        acc.eo = endOffset - acc.tl;
+      }
 
+      acc.tl += l;
       return acc;
     },
     {
       tl: 0,
       s: -1,
-      e: -1
+      e: -1,
+      so: 0,
+      eo: 0
     }
   );
+};
 
 const createWordRange = () => {
   const range = window.getSelection().getRangeAt(0);
@@ -39,7 +48,7 @@ const createWordRange = () => {
   const text = line.innerText;
   const parentElement = getRootParent(startContainer);
   const parentElementIndex = lineChildren.indexOf(parentElement);
-  const containerOffset = mapNodesToTextLength(lineChildren).reduce(
+  const containerOffset = mapTextToLength(mapNodesToText(lineChildren)).reduce(
     (acc, val, i) => (i < parentElementIndex ? acc + val : acc),
     0
   );
@@ -48,8 +57,8 @@ const createWordRange = () => {
   // @ts-ignore
   const rh = text.substring(containerOffset + caretOffset);
 
-  let lw: any = /\b[A-Za-z]+$/.exec(lh);
-  let rw: any = /^[A-Za-z]+\b/.exec(rh);
+  let lw: any = /\b[0-9_A-Za-z]+$/.exec(lh);
+  let rw: any = /^[0-9_A-Za-z]+\b/.exec(rh);
   if (lw && !rw) rw = { 0: '', index: 0 };
   if (!lw && rw) lw = { 0: '', index: lh.length };
   if (!lw && !rw) throw Error('No adjacent word');
@@ -57,7 +66,7 @@ const createWordRange = () => {
   const word = lw[0] + rw[0];
   const startOffset = lw['index'];
   const endOffset = startOffset + word.length;
-  const { s, e } = findAnchors({
+  const { s, e, so, eo } = findAnchors({
     line,
     startOffset,
     endOffset
@@ -66,9 +75,8 @@ const createWordRange = () => {
   return {
     startElement: line.childNodes[s],
     endElement: line.childNodes[e],
-    startOffset: startOffset - containerOffset,
-    endOffset: endOffset - containerOffset,
-    containerOffset
+    startOffset: so, //startOffset - (s > 0 ? containerOffset : 0),
+    endOffset: eo, //endOffset - containerOffset,
   };
 };
 const getElementOfNode = node =>
