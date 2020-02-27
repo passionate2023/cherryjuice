bebaf32e56f1262970dfdf764589418ef690ad15
Yacine Mohamdi <myacine70@gmail.com>
2020-02-27 21:11:41 +0100

    (execK) revamp apply-tag logic to mimic ct

--- cs_client/src/helpers/execK/steps/apply-command/apply-tag/index.ts
+++ cs_client/src/helpers/execK/steps/apply-command/apply-tag/index.ts
@@ -1,20 +1,28 @@
-const applyTag = ({ aHtmlElement, tagExists, tagName }) => {
-  const toBeDeleted = aHtmlElement.tags.find(([tag]) => tag === tagName);
-  if (tagExists) {
-    if (toBeDeleted) {
-      const indexOfTagToBeDeleted = aHtmlElement.tags.indexOf(toBeDeleted);
-      aHtmlElement.tags.splice(indexOfTagToBeDeleted, 1);
-      // merge styles of the item to be deleted with the top-level tag tag
-      if (!aHtmlElement.tags.length) aHtmlElement.tags.push(['span', {}]);
-      if (toBeDeleted[1].style)
-        aHtmlElement.tags[0][1].style = {
-          ...toBeDeleted[1].style,
-          ...(aHtmlElement.tags[0][1].style || {})
-        };
-    }
-  } else {
-    if (!toBeDeleted) aHtmlElement.tags.push([tagName, {}]);
-  }
+import { calculateTag } from '::helpers/execK/steps/apply-command/apply-tag/calculate-tag';
+
+const mergeStylesWithRootTag = ({ aHtmlElement, tagToBeDeleted }) => {
+  if (!aHtmlElement.tags.length) aHtmlElement.tags.push(['span', {}]);
+  if (tagToBeDeleted[1].style)
+    aHtmlElement.tags[0][1].style = {
+      ...tagToBeDeleted[1].style,
+      ...(aHtmlElement.tags[0][1].style || {})
+    };
+};
+const applyTag = ({ aHtmlElement, tagExists: remove, tagName }) => {
+  // const tagToBeDeleted = aHtmlElement.tags.find(([tag]) => tag === tagName);
+  // if (remove) {
+  //   if (tagToBeDeleted) {
+  //     const indexOfTagToBeDeleted = aHtmlElement.tags.indexOf(tagToBeDeleted);
+  //     aHtmlElement.tags.splice(indexOfTagToBeDeleted, 1);
+  //     mergeStylesWithRootTag({ aHtmlElement,  tagToBeDeleted });
+  //   }
+  // } else {
+  //   if (!tagToBeDeleted) aHtmlElement.tags.push([tagName, {}]);
+  // }
+  aHtmlElement.tags = calculateTag({
+    cmd: { remove, tagName },
+    tags: aHtmlElement.tags
+  });
 };
 
 export { applyTag };
