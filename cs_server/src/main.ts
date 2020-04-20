165db2c70bc30ea73c2be84cc4a050e82e9fbf96
Yacine Mohamdi <myacine70@gmail.com>
2020-04-20 07:45:59 +0100

    build: setup webpack hmr

--- cs_server/src/main.ts
+++ cs_server/src/main.ts
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
@@ -9,6 +9,12 @@ async function bootstrap() {
   await app.listen(port);
   // eslint-disable-next-line no-console
   console.log(`Listening on port ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 // eslint-disable-next-line no-console
 bootstrap().catch(console.error);
--- cs_server/webpack.config.js
+++ cs_server/webpack.config.js
@@ -1,18 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const NodemonPlugin = require('nodemon-webpack-plugin');
+const StartServerPlugin = require('start-server-webpack-plugin');
+
 module.exports = {
+  entry: ['webpack/hot/poll?100', './src/main.ts'],
   watch: true,
-  mode: "development",
-  devtool: "inline-source-map",
   target: 'node',
+
+  mode: 'development',
+  devtool: 'inline-source-map',
   externals: [
     nodeExternals({
-      whitelist: ['webpack/hot/poll?1000'],
+      whitelist: ['webpack/hot/poll?100'],
     }),
   ],
-  entry: './src/main.ts',
   module: {
     rules: [
       {
@@ -30,11 +34,12 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'server.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new NodemonPlugin(),
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+    new StartServerPlugin({ name: 'server.js' }),
+  ],
 };
