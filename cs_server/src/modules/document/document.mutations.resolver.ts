passionate2023/cherryjuice

commit 163612b9cadf18651f387df53dac7f323c0b391c

fix: update node creation/modification time
fix #cs-62

cs_server/src/modules/document/document.mutations.resolver.ts
@@ -76,7 +76,9 @@ export class DocumentMutationsResolver {
   async node(
     @Parent() parent,
     @Args('node_id', { type: () => Int }) node_id: number,
+    @GetUserGql() user: User,
   ) {
+    await this.documentService.getDocumentMetaById(user, parent.id);
     return { node_id, documentId: parent.id };
   }
 

cs_server/src/modules/imports/imports.service.ts
@@ -35,7 +35,7 @@ export type DocumentDTO = {
   // id: string;
 };
 type ImageIdsMap = { [tempId: string]: string };
-
+type NodeDatesMap = Map<number, { createdAt: Date; updatedAt: Date }>;
 @Injectable()
 export class ImportsService {
   constructor(
@@ -64,6 +64,7 @@ export class ImportsService {
   ) {
     const nodesWithImages: Node[] = [];
     const nodesMap = new Map<number, Node>();
+    const nodeDatesMap: NodeDatesMap = new Map();
     const rawNodesMap: Map<number, Node> = new Map(
       rawNodes.map(node => [node.node_id, node]),
     );
@@ -83,6 +84,10 @@ export class ImportsService {
       node.document = newDocument;
       nodesMap.set(node.node_id, node);
       await node.save();
+      nodeDatesMap.set(node.node_id, {
+        createdAt: new Date(nodeRaw.createdAt),
+        updatedAt: new Date(nodeRaw.updatedAt),
+      });
       if (nodeRaw.has_image) nodesWithImages.push(node);
     }
 
@@ -94,20 +99,24 @@ export class ImportsService {
       const father = nodesMap.get(node.father_id);
       if (father) node.father = father;
     }
-    return { nodesWithImages, nodesMap };
+    return { nodesWithImages, nodesMap, nodeDatesMap };
   }
 
   private async saveNodes(
     newDocument: Document,
-  ): Promise<{ nodesWithImages: Node[]; nodesMap: Map<number, Node> }> {
+  ): Promise<{
+    nodesWithImages: Node[];
+    nodesMap: Map<number, Node>;
+    nodeDatesMap: NodeDatesMap;
+  }> {
     const rawNodes = (await this.nodeSqliteRepository.getNodesMeta(
       false,
     )) as (Node & { is_ro; has_image })[];
-    const { nodesWithImages, nodesMap } = await this.functiona(
+    const { nodesWithImages, nodesMap, nodeDatesMap } = await this.functiona(
       newDocument,
       rawNodes,
     );
-    return { nodesWithImages, nodesMap };
+    return { nodesWithImages, nodesMap, nodeDatesMap };
   }
   private async saveDocument({
     document,
@@ -118,9 +127,11 @@ export class ImportsService {
   }): Promise<void> {
     const filePath = '/uploads/' + document.name;
     await this.openUploadedFile(filePath);
-    const { nodesWithImages, nodesMap } = await this.saveNodes(document);
+    const { nodesWithImages, nodesMap, nodeDatesMap } = await this.saveNodes(
+      document,
+    );
     const { imagesOfNodes } = await this.saveImages(nodesWithImages);
-    await this.saveAhtml({ imagesOfNodes, nodesMap });
+    await this.saveAhtml({ imagesOfNodes, nodesMap, nodeDatesMap });
     const { size } = fs.statSync(filePath);
     document.size = size;
     document.hash = hash;
@@ -265,9 +276,11 @@ export class ImportsService {
   private async saveAhtml({
     nodesMap,
     imagesOfNodes,
+    nodeDatesMap,
   }: {
     nodesMap: Map<number, Node>;
     imagesOfNodes: Map<number, Image[]>;
+    nodeDatesMap: NodeDatesMap;
   }) {
     for (const node of Array.from(nodesMap.values())) {
       node.ahtml = JSON.stringify(
@@ -276,6 +289,8 @@ export class ImportsService {
           imagesOfNodes.get(node.node_id),
         ),
       );
+      node.createdAt = nodeDatesMap.get(node.node_id).createdAt;
+      node.updatedAt = nodeDatesMap.get(node.node_id).updatedAt;
       await node.save();
     }
   }

cs_server/src/modules/node/repositories/node.sqlite.repository.ts
@@ -52,7 +52,8 @@ const queries = {
     WHERE node_id = ${node_id}`,
   },
 };
-
+const convertTime = data =>
+  Math.round((new Date(data * 1000) as unknown) as number);
 @Injectable()
 export class NodeSqliteRepository {
   constructor(private documentSqliteRepository: DocumentSqliteRepository) {}
@@ -87,6 +88,8 @@ export class NodeSqliteRepository {
           ...node,
           child_nodes: [],
           is_empty: 0,
+          createdAt: convertTime(node.createdAt),
+          updatedAt: convertTime(node.updatedAt),
         })),
       );
 

cs_client/src/components/app/editor/document/hooks/get-document-meta/helpers/construct-tree.ts
@@ -25,6 +25,7 @@ const applyLocalModifications = ({
     nodes = new Map(nodes);
     const modifiedNodes = [
       ...apolloCache.changes.node.meta.map(([nodeId]) => nodeId),
+      ...apolloCache.changes.node.html,
       ...apolloCache.changes.node.created,
     ];
     for (const nodeId of modifiedNodes) {

cs_client/src/components/app/editor/document/hooks/save-document/helpers/save-nodes-content.ts
@@ -37,8 +37,11 @@ const saveNodesContent = async ({ state }: SaveOperationProps) => {
       variables: {
         file_id: node.documentId,
         node_id: node.node_id,
-        ahtml: JSON.stringify(aHtml),
-        deletedImages: deletedImages || [],
+        data: {
+          ahtml: JSON.stringify(aHtml),
+          deletedImages: deletedImages || [],
+          updatedAt: node.updatedAt,
+        },
       },
     });
     apolloCache.changes.unsetModificationFlag(

cs_client/src/components/app/editor/document/hooks/save-document/helpers/save-nodes-meta.ts
@@ -31,6 +31,7 @@ const saveNodesMeta = async ({ state }: SaveOperationProps) => {
     editedAttributes.forEach(attribute => {
       meta[attribute] = node[attribute];
     });
+    meta['updatedAt'] = node.updatedAt;
     swapFatherIdIfApplies(state)(node);
     if (collectDanglingNodes(state)(node)) continue;
     updateDocumentId(state)(node);

cs_client/src/components/app/editor/document/index.tsx
@@ -16,7 +16,7 @@ import { Store } from '::root/store';
 import { connect, ConnectedProps } from 'react-redux';
 import { ac } from '::root/store/actions.types';
 import { setHighestNodeId } from '::app/editor/document/hooks/get-document-meta/helpers/set-highset-node_id';
-import { navigate } from '::root/router/navigate';
+import { router } from '::root/router/router';
 import { asyncOperation } from '::root/store/ducks/document';
 
 const mapState = (state: Store) => ({
@@ -59,13 +59,13 @@ const Document: React.FC<Props & PropsFromRedux> = ({
     setHighestNodeId(nodes);
   }, [nodes]);
   useEffect(() => {
-    if (selectedNode.node_id) navigate.node(file_id, selectedNode.node_id);
-  }, [selectedNode.node_id]);
+    if (selectedNode.node_id) router.node(file_id, selectedNode.node_id);
+  }, [selectedNode.node_id, file_id]);
   useTrackDocumentChanges({ cacheTimeStamp });
   useEffect(() => {
-    if (navigate.location.pathname.endsWith(file_id))
+    if (router.location.pathname.endsWith(file_id))
       ac.document.clearSelectedNode();
-  }, [navigate.location.pathname]);
+  }, [router.location.pathname]);
 
   useEffect(() => {
     ac.document.setDocumentId(file_id);

cs_client/src/components/app/editor/document/recent-nodes/recent-nodes.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useCallback } from 'react';
 import { modRecentNodes } from '::sass-modules/index';
 import { updateCachedHtmlAndImages } from '::app/editor/document/tree/node/helpers/apollo-cache';
-import { navigate } from '::root/router/navigate';
+import { router } from '::root/router/router';
 import { NodeMeta } from '::types/graphql/adapters';
 import { nodesMetaMap } from '::types/misc';
 
@@ -41,7 +41,7 @@ const RecentNodes: React.FC<Props> = ({
     e => {
       updateCachedHtmlAndImages();
       const node_id = e.target.dataset.id;
-      navigate.node(file_id, node_id);
+      router.node(file_id, node_id);
     },
     [file_id],
   );

commit 591bccf3564ebe32de1b19b00f393b93ccc8256f

fix(import-document): fix regression caused by 163612b9

cs_server/src/modules/document/document.mutations.resolver.ts
@@ -72,15 +72,6 @@ export class DocumentMutationsResolver {
     const deleteResult = await this.documentService.deleteDocuments(IDs, user);
     return JSON.stringify(deleteResult);
   }
-  @ResolveField(() => [NodeMutation])
-  async node(
-    @Parent() parent,
-    @Args('node_id', { type: () => Int }) node_id: number,
-    @GetUserGql() user: User,
-  ) {
-    await this.documentService.getDocumentMetaById(user, parent.id);
-    return { node_id, documentId: parent.id };
-  }
 
   @ResolveField(() => String)
   async createDocument(
@@ -97,5 +88,15 @@ export class DocumentMutationsResolver {
       user,
     });
     return createResult.id;
+  }
+
+  @ResolveField(() => [NodeMutation])
+  async node(
+    @Parent() parent,
+    @Args('node_id', { type: () => Int }) node_id: number,
+    @GetUserGql() user: User,
+  ) {
+    await this.documentService.getDocumentMetaById(user, parent.id);
+    return { node_id, documentId: parent.id };
   }
 }

cs_server/src/modules/imports/imports.service.ts
@@ -58,7 +58,7 @@ export class ImportsService {
     await this.documentSqliteRepository.closeUploadedFile();
   }
 
-  async functiona(
+  async saveNodesMeta(
     newDocument: Document,
     rawNodes: (Node & { is_ro; has_image })[],
   ) {
@@ -80,7 +80,7 @@ export class ImportsService {
       nodeRaw.icon_id = '' + nodeTitleHelpers.customIconId(nodeRaw.is_ro);
 
       const node = new Node();
-      copyProperties(nodeRaw, node, {});
+      copyProperties(nodeRaw, node, { createdAt: true, updatedAt: true });
       node.document = newDocument;
       nodesMap.set(node.node_id, node);
       await node.save();
@@ -112,10 +112,11 @@ export class ImportsService {
     const rawNodes = (await this.nodeSqliteRepository.getNodesMeta(
       false,
     )) as (Node & { is_ro; has_image })[];
-    const { nodesWithImages, nodesMap, nodeDatesMap } = await this.functiona(
-      newDocument,
-      rawNodes,
-    );
+    const {
+      nodesWithImages,
+      nodesMap,
+      nodeDatesMap,
+    } = await this.saveNodesMeta(newDocument, rawNodes);
     return { nodesWithImages, nodesMap, nodeDatesMap };
   }
   private async saveDocument({

cs_server/src/modules/node/repositories/node.sqlite.repository.ts
@@ -104,8 +104,8 @@ export class NodeSqliteRepository {
       has_codebox: 0,
       has_table: 0,
       sequence: 0,
-      createdAt: 0,
-      updatedAt: 0,
+      createdAt: new Date().getTime(),
+      updatedAt: new Date().getTime(),
       child_nodes: [],
       is_empty: 0,
       node_title_styles: '',