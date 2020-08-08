33ebacbd6d582c2490390683b89fa545222b6b77
Yacine Mohamdi <myacine70@gmail.com>
2020-08-08 01:33:38 +0100

    improvement(emails): improve email templates

--- cs_server/src/modules/email/email.service.ts
+++ cs_server/src/modules/email/email.service.ts
@@ -14,10 +14,11 @@ export class EmailService {
     const url = `${process.env.ASSETS_URL}/reset-password#token=${token}`;
     await this.mailerService.sendMail({
       to: email,
-      subject: 'Password reset',
-      text: 'url to reset your password: ' + url,
+      subject: 'Password reset request',
+      text: `Hello, someone has requested a password reset for the Cherryscript account associated with ${email}\nto reset your password follow this link: ${url}\nIf you didn't make this request, ignore this email.`,
       context: {
         resetPasswordUrl: url,
+        email,
       },
       template: 'reset-password',
     });
@@ -27,8 +28,8 @@ export class EmailService {
     const url = `${process.env.ASSETS_URL}/verify-email#token=${token}`;
     await this.mailerService.sendMail({
       to: email,
-      subject: 'Verify your email',
-      text: 'url to verify your email: ' + url,
+      subject: 'Confirm Your Email',
+      text: `Hello, to confirm your email, follow this link: ${url}`,
       context: {
         verifyEmailUrl: url,
       },
