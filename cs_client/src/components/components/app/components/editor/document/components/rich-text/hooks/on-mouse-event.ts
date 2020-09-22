5b957b5b233fb7c854408166546c97e348de88d9
Yacine Mohamdi <myacine70@gmail.com>
2020-09-22 13:08:29 +0100

    fix(editor): links don't work properly in touch devices

--- cs_client/src/components/components/app/components/editor/document/components/rich-text/hooks/on-mouse-event.ts
+++ cs_client/src/components/components/app/components/editor/document/components/rich-text/hooks/on-mouse-event.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { modRichText } from '::sass-modules';
-import { fromEvent, merge } from 'rxjs';
-import { debounceTime, filter, map, tap, withLatestFrom } from 'rxjs/operators';
+import { fromEvent, merge, Observable } from 'rxjs';
+import { debounceTime, filter, map, withLatestFrom } from 'rxjs/operators';
 import { ac, store } from '::store/store';
 import { onLinkClicked } from '::root/components/app/components/editor/document/components/rich-text/helpers/links';
 
@@ -12,71 +12,83 @@ const watchedElements = {
   a: true,
   img: true,
 };
+
+const clickify$ = (
+  start$: Observable<Event>,
+  end$: Observable<Event>,
+): [Observable<[Event, number]>, Observable<[Event, number]>] => {
+  const state = {
+    latestMouseDownTimeStamp: 0,
+  };
+  const click$ = end$.pipe(
+    withLatestFrom(start$),
+    filter(tuple => tuple[1].timeStamp !== state.latestMouseDownTimeStamp),
+    map<[Event, Event], [Event, number]>(tuple => {
+      const [up, down] = tuple;
+      state.latestMouseDownTimeStamp = down.timeStamp;
+      return [down, up.timeStamp - down.timeStamp];
+    }),
+  );
+
+  const hold$ = start$.pipe(
+    debounceTime(300),
+    filter(e => e['timeStamp'] !== state.latestMouseDownTimeStamp),
+    map<Event, [Event, number]>(down => {
+      state.latestMouseDownTimeStamp = down.timeStamp;
+      return [down, 300];
+    }),
+  );
+
+  return [click$, hold$];
+};
+
+const getValidTarget = (e: Event): HTMLElement =>
+  Array.from(e['path']).find(
+    el => watchedElements[el['localName']],
+  ) as HTMLElement;
+
+const isLeftClick = (e: MouseEvent): boolean => e['which'] === 1;
+
 export const useMouseClick = () => {
   useEffect(() => {
-    const state = {
-      latestMouseDownTimeStamp: 0,
-    };
     const element = document.querySelector(
       `.${modRichText.richText__container}`,
     );
-    const simpleClick = fromEvent(element, 'click')
-      .pipe(
-        filter(e => e['which'] === 1),
-        tap(e => e.preventDefault()),
-      )
-      .subscribe();
-    const md$ = fromEvent(element, 'mousedown').pipe(
-      filter(e => e['which'] === 1),
-      tap(e => e.preventDefault()),
-    );
-    const mu$ = fromEvent(element, 'mouseup').pipe(
-      filter(e => e['which'] === 1),
-      tap(e => e.preventDefault()),
-    );
-    const click$ = mu$.pipe(
-      withLatestFrom(md$),
-      filter(tuple => tuple[1].timeStamp !== state.latestMouseDownTimeStamp),
-      map<[Event, Event], [Event, number]>(tuple => [
-        tuple[1],
-        tuple[0].timeStamp - tuple[1].timeStamp,
-      ]),
-    );
+    const md$ = fromEvent(element, 'mousedown').pipe(filter(isLeftClick));
+    const mu$ = fromEvent(element, 'mouseup').pipe(filter(isLeftClick));
+    const [mouseClick$, mouseHold$] = clickify$(md$, mu$);
 
-    const hold$ = md$.pipe(
-      debounceTime(300),
-      filter(e => e.timeStamp !== state.latestMouseDownTimeStamp),
-      map<Event, [Event, number]>(e => [e, 300]),
-    );
-
-    const subscription = merge(click$, hold$).subscribe(
-      ([e, clickDuration]) => {
-        state.latestMouseDownTimeStamp = e.timeStamp;
-        const target = Array.from(e['path']).find(
-          el => watchedElements[el['localName']],
-        ) as HTMLElement;
-        if (target) {
-          e.preventDefault();
-          if (clickDuration >= 300) {
-            if (target.className === anchor) {
-              const id = target.getAttribute('id');
-              ac.editor.setAnchorId(id);
-              ac.dialogs.showAnchorDialog();
-            }
-          } else {
-            if (
-              target.classList[0] === link ||
-              target.classList.contains(imageLink)
-            ) {
-              onLinkClicked(target, store.getState().document.documentId);
-            }
+    const ts$ = fromEvent(element, 'touchstart');
+    const te$ = fromEvent(element, 'touchend');
+    const [touchClick$, touchHold$] = clickify$(ts$, te$);
+    const subscription = merge(
+      mouseClick$,
+      mouseHold$,
+      touchClick$,
+      touchHold$,
+    ).subscribe(([e, clickDuration]) => {
+      const target = getValidTarget(e);
+      if (target) {
+        e.preventDefault();
+        if (clickDuration >= 300) {
+          const isAnchor = target.className === anchor;
+          if (isAnchor) {
+            const id = target.getAttribute('id');
+            ac.editor.setAnchorId(id);
+            ac.dialogs.showAnchorDialog();
+          }
+        } else {
+          const isSimpleLink = target.classList[0] === link;
+          const isImageLink =
+            !isSimpleLink && target.classList.contains(imageLink);
+          if (isSimpleLink || isImageLink) {
+            onLinkClicked(target, store.getState().document.documentId);
           }
         }
-      },
-    );
+      }
+    });
     return () => {
       subscription.unsubscribe();
-      simpleClick.unsubscribe();
     };
   }, []);
 };
