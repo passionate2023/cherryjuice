fa970096835a14d8e8a3b39e641319605c0c94e1
Yacine Mohamdi <myacine70@gmail.com>
2020-11-18 16:39:00 +0100

    fix: node-meta icon picker hidden by dialog footer

--- apps/cs_client/src/components/components/app/components/menus/dialogs/node-meta/components/icon-picker.tsx
+++ apps/cs_client/src/components/components/app/components/menus/dialogs/node-meta/components/icon-picker.tsx
@@ -1,81 +1,54 @@
 import * as React from 'react';
-import { modIconPicker, modNodeMeta } from '::sass-modules';
+import { modIconPicker, modNodeMeta, modSearchFilter } from '::sass-modules';
 import { Icon, Icons } from '::root/components/shared-components/icon/icon';
 import { useState } from 'react';
-import { useClickOutsideModal } from '::hooks/use-click-outside-modal';
 import { testIds } from '::cypress/support/helpers/test-ids';
-
-const icons = [...Object.entries(Icons.cherrytree.custom_icons)];
-
-const IconsList = ({
-  selectedIcon,
-  setSelectedIcon,
-  setShowList,
-  showList,
-}) => {
-  useClickOutsideModal({
-    callback: () => setShowList(false),
-    selector: '.' + modIconPicker.iconPicker__icon__list,
-  });
-
-  return (
-    <div
-      className={`${modIconPicker.iconPicker__icon__list} ${
-        !showList ? modIconPicker.iconPicker__icon__listHidden : ''
-      }`}
-      data-testid={testIds.nodeMeta__customIconList}
-    >
-      {icons.map(([iconId, iconName], i) => (
-        <Icon
-          name={iconName}
-          className={`${modIconPicker.iconPicker__icon} ${
-            selectedIcon === iconId
-              ? modIconPicker.iconPicker__iconSelected
-              : ''
-          }`}
-          key={i}
-          onClick={() => {
-            setSelectedIcon(iconId);
-            setShowList(false);
-          }}
-        />
-      ))}
-    </div>
-  );
-};
+import { ContextMenuWrapper } from '::root/components/shared-components/context-menu/context-menu-wrapper';
+import { IconsList } from '::root/components/app/components/menus/dialogs/node-meta/components/components/icons-list';
 
 type Props = { value: string; disabled: boolean; onChange };
 
 const IconPicker: React.FC<Props> = ({
-  value: selectedIcon,
   disabled,
+  value: selectedIcon,
   onChange: setSelectedIcon,
 }) => {
-  const [showList, setShowList] = useState(false);
+  const [shown, setShown] = useState(false);
+  const show = () => setShown(true);
+  const hide = () => setShown(false);
   const value = selectedIcon === '0' ? '1' : selectedIcon;
+
   return (
-    <div
-      className={modIconPicker.iconPicker}
-      data-testid={testIds.nodeMeta__customIcon}
-    >
-      <Icon
-        name={Icons.cherrytree.custom_icons[value]}
-        className={`${modIconPicker.iconPicker__icon} ${
-          disabled ? modNodeMeta.nodeMeta__inputDisabled : ''
-        }`}
-        onClick={() => !disabled && setShowList(!showList)}
-      />
-      {showList && (
+    <ContextMenuWrapper
+      clickOutsideSelectorsWhitelist={['.' + modSearchFilter.searchFilter]}
+      showAsModal={'mb'}
+      shown={shown}
+      hide={hide}
+      show={show}
+      customBody={
         <IconsList
           {...{
             selectedIcon,
             setSelectedIcon,
-            setShowList,
-            showList,
+            hide,
+            shown,
           }}
         />
-      )}
-    </div>
+      }
+    >
+      <div
+        className={modIconPicker.iconPicker}
+        data-testid={testIds.nodeMeta__customIcon}
+      >
+        <Icon
+          name={Icons.cherrytree.custom_icons[value]}
+          className={`${modIconPicker.iconPicker__icon} ${
+            disabled ? modNodeMeta.nodeMeta__inputDisabled : ''
+          }`}
+          onClick={() => !disabled && setShown(shown => !shown)}
+        />
+      </div>
+    </ContextMenuWrapper>
   );
 };
 
