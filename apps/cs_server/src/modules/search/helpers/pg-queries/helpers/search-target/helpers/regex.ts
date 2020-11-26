9c618d1ad23c88b44a4ba2797780660eda921be9
Yacine Mohamdi <myacine70@gmail.com>
2020-11-26 16:06:39 +0100

    improvement: add support for regex flags

--- apps/cs_server/src/modules/search/helpers/pg-queries/helpers/search-target/helpers/regex.ts
+++ apps/cs_server/src/modules/search/helpers/pg-queries/helpers/search-target/helpers/regex.ts
@@ -1,13 +1,24 @@
 import { HeadlineProps, SearchTypeQueryProps } from './fts';
+import { FlagsPositionInVariablesList } from '../search-target';
 
 const whereClause = ({
   searchOptions,
   variableIndex,
-}: SearchTypeQueryProps): string =>
-  `~${searchOptions.caseSensitive ? '' : '*'} $${variableIndex}`;
+  queryHasFlags,
+}: SearchTypeQueryProps & { queryHasFlags: boolean }): string =>
+  `~${searchOptions.caseSensitive ? '' : '*'} $${variableIndex -
+    (queryHasFlags ? FlagsPositionInVariablesList.flagsAndQuery : 0)}`;
 
-const headline = ({ columnName, numberOfVariables }: HeadlineProps): string =>
-  `(regexp_matches(${columnName}, $${numberOfVariables}))[1]`;
+const headline = ({
+  columnName,
+  numberOfVariables,
+  queryHasFlags,
+}: HeadlineProps & { queryHasFlags: boolean }): string =>
+  `(regexp_matches(${columnName}, $${numberOfVariables}${
+    queryHasFlags
+      ? `, $${numberOfVariables - FlagsPositionInVariablesList.flags}`
+      : ''
+  }))[1]`;
 
 const regex = {
   whereClause: whereClause,
