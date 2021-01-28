8c3989c83e1ef2135858353572b210aa375a5738
Yacine Mohamdi <myacine70@gmail.com>
2021-01-28 21:51:21 +0100

    improvement(inline-input): auto width

--- apps/cs_client/src/components/components/shared-components/inline-input/inline-input.tsx
+++ apps/cs_client/src/components/components/shared-components/inline-input/inline-input.tsx
@@ -5,27 +5,23 @@ import {
   useOnKeyPress,
 } from '@cherryjuice/shared-helpers';
 import mod from './inline-input.scss';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import {
   CheckValidity,
   OnAcceptInput,
 } from '::shared-components/inline-input/hooks/inline-input-provider';
 
 type Props = {
-  autoFocus?: boolean;
   initialValue: string;
   className?: string;
   onAcceptInput: OnAcceptInput;
   checkValidity: CheckValidity;
-  width?: number;
 };
 export const InlineInput: React.FC<Props> = ({
   checkValidity,
-  autoFocus = true,
   onAcceptInput,
   initialValue,
   className,
-  width,
 }) => {
   const [valid, setValid] = useState(true);
   const [value, setValue] = useState(initialValue);
@@ -43,21 +39,39 @@ export const InlineInput: React.FC<Props> = ({
     callback: acceptInput,
   });
   useOnKeyPress({ ref, onClick: acceptInput, keys: ['Enter'] });
-
+  useLayoutEffect(() => {
+    setValid(value === initialValue ? true : checkValidity(value));
+  }, [value]);
+  useEffect(() => {
+    ref.current.innerHTML = value;
+    ref.current.focus();
+    try {
+      const range = document.createRange();
+      range.setStart(ref.current.firstChild, value.length);
+      const sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+      // eslint-disable-next-line no-empty
+    } catch {}
+  }, []);
   return (
-    <input
+    <span
       {...clkOProps}
-      type="text"
+      contentEditable={true}
       className={joinClassNames([
         className,
         mod.inlineInput,
         [mod.inlineInputInvalid, !valid],
       ])}
-      style={{ width }}
-      value={value}
-      onChange={e => setValue(e.target.value)}
+      role={'textarea'}
+      onKeyUp={() => setValue(ref.current.innerText)}
+      onKeyDown={e => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }}
       ref={ref}
-      autoFocus={autoFocus}
       onDoubleClick={e => {
         e.preventDefault();
         e.stopPropagation();
