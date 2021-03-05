fbc09faeedbdd8613c1a21497190a9edd376dba0
Yacine Mohamdi <myacine70@gmail.com>
2021-03-05 16:33:59 +0100

    chore: setup @swc/jest

--- libs/cs_editor/jest.config.js
+++ libs/cs_editor/jest.config.js
@@ -3,24 +3,11 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 // eslint-disable-next-line no-undef
 const tsconfig = require('./tsconfig');
+const config = require('../../config/jest/jest.config');
 // eslint-disable-next-line no-undef
 module.exports = {
-  roots: [/*'<rootDir>/.storybook', */ '<rootDir>/src'],
-  preset: 'ts-jest/presets/js-with-ts',
+  ...config,
   testEnvironment: 'jsdom',
-
-  // jest.config.js
-  // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-  // which contains the path mapping (ie the `compilerOptions.paths` option):
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json',
-      babelConfig: false,
-      diagnostics: {
-        warnOnly: true,
-      },
-    },
-  },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
@@ -29,5 +16,4 @@ module.exports = {
       prefix: '<rootDir>/',
     }),
   },
-  testPathIgnorePatterns: ['/__data__/', '/__tests__/data/', '__helpers__'],
 };
